3af44edf755764473ad358c683f7c7f13e74a7db
Glauco Dante <108493608+glaucodante@users.noreply.github.com>
2022-10-18 11:43:21 -0300

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 import "./css/index.css"
+//cc = cartão de crédito
+// querySelector = busque pelo seletor
+// nth-child(1) = está pegando o 1º g do html
+// child = filho
+// Alterando as cores do cartão de acordo com as propriedades abaixo
+const ccbgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccbgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+// Pegando a logo
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+// Estrutura de dados para as cores do cartão
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  }
+  ccbgColor01.setAttribute("fill", colors[type][0])
+  ccbgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+// função que ativa a funcionalidade de mudar as cores e logo
+setCardType("mastercard")
+// globalThis.setCardType = setCardType
